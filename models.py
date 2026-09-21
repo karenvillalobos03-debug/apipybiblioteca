ff6b4c9fed8532e06aa1c649af86f6635ca0f22a
karenvillalobos03-debug <karen.villalobos03@unach.mx>
2026-09-21 12:01:07 -0600

    Update models.py

--- models.py
+++ models.py
@@ -1,10 +1,21 @@
 from pydantic import BaseModel, Field
+from typing import Optional
 
-class Book(BaseModel):
-    title: str = Field(
-        ..., min_length=1, max_length=100
-    )
-    author: str = Field(
-        ..., min_length=1, max_length=50
-    )
-    year: int = Field(..., gt=1900, lt=2100)
+class Editorial(BaseModel):
+    idEd: int = Field(
+        ..., gt=0, description="Identificador único de la editorial")
+    nombre: str = Field(
+        ..., min_length=1, max_length=100)
+    pais: str = Field(
+        ..., min_length=1, max_length=50)
+
+class Libro(BaseModel):
+    isbn: str = Field(
+        ..., min_length=10, max_length=13, description="Código ISBN")
+    titulo: str = Field(
+        ..., min_length=1, max_length=100)
+    autor: str = Field(
+        ..., min_length=1, max_length=50)
+    precio: float = Field(
+        ..., gt=0.0, description="Precio del libro")
+    editorial: Optional[Editorial] = None
